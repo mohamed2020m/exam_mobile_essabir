63fc33afc413c014d55683bded6b8f730ba4f4e7
Leeuw <mohamedessabir20@gmail.com>
2023-11-15 22:48:40 +0100

    First commit

--- prof-backend/src/main/java/com/leeuw/repositories/SpecialiteRepository.java
+++
@@ -1,6 +0,0 @@
-package com.leeuw.repositories;
-
-import com.leeuw.entities.Specialite;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SpecialiteRepository extends JpaRepository<Specialite, Long> {}
